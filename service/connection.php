bf08fd11c83a49681e88c88b8516f781086353a0
0rcaaa <iamsyarel@gmail.com>
2025-03-02 17:22:44 +0700

    config views page

--- service/connection.php
+++ service/connection.php
@@ -1,5 +1,5 @@
 <?php
-$conn = mysqli_connect("localhost","0rca","0_rcarius","kasir");
+$conn = mysqli_connect("localhost","0rca","0rcarius","kasir");
 if (mysqli_connect_errno()) {
     printf("", mysqli_connect_error());
     exit(1);
